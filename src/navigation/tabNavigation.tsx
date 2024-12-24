fffbf8e3ff193ffd240cff49a8dc1b0a2ad426e2
Victor Aponte <vaponte520@gmail.com>
2024-12-24 19:01:43 -0300

    Agrega: cambia los nombre del los tabnavigation

--- src/navigation/tabNavigation.tsx
+++ src/navigation/tabNavigation.tsx
@@ -3,13 +3,15 @@ import {Routes} from './routes';
 
 //Views
 import UserFormScreen from '../screens/Register';
+import LoginScreen from '../screens/Login';
 import HomeScreen from '../screens/Home';
 
 const Stack = createNativeStackNavigator();
 const TabNavigation = () => {
   return (
-    <Stack.Navigator initialRouteName={Routes.HOMESCREEN}>
-      <Stack.Screen name={Routes.HOMESCREEN} component={HomeScreen} />
+    <Stack.Navigator initialRouteName={Routes.LOGIN_SCREEN}>
+      <Stack.Screen name={Routes.LOGIN_SCREEN} component={LoginScreen} />
+      <Stack.Screen name={Routes.HOME_SCREEN} component={HomeScreen} />
       <Stack.Screen name={Routes.USER_RESGISTER} component={UserFormScreen} />
     </Stack.Navigator>
   );
