6627e04d1e135541f17589d2a457371a2b602bfb
Victor Aponte <vaponte520@gmail.com>
2024-12-24 19:02:51 -0300

    Agrega: stilos propios de login vista

---
+++ src/screens/Login/styles.tsx
@@ -0,0 +1,45 @@
+import {StyleSheet} from 'react-native';
+import {colors} from '../../assets/theme';
+
+const styles = StyleSheet.create({
+  buttomContainerLogin: {
+    backgroundColor: colors.greenLith,
+    paddingVertical: 12,
+    borderRadius: 5,
+    top: 10,
+  },
+  buttomContainerRegister: {
+    marginTop: 40,
+  },
+  IconLogins: {
+    width: '100%',
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'white',
+    paddingVertical: 5,
+    borderRadius: 5,
+    marginBottom: 10,
+    borderColor: 'gray',
+    borderWidth: 0.2,
+  },
+  textLogin: {
+    color: colors.white,
+    textAlign: 'center',
+    fontSize: 16,
+    fontWeight: '600',
+  },
+  textRegister: {
+    color: colors.greenLith,
+    textAlign: 'center',
+    fontSize: 14,
+    fontWeight: '600',
+  },
+  containerRedes: {
+    flex: 1,
+    justifyContent: 'center',
+    top: 30,
+  },
+});
+
+export default styles;
