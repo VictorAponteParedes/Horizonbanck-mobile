fa7440626fd8a1f802a2c7c07f39bc94888d7579
Victor Aponte <vaponte520@gmail.com>
2024-12-24 19:03:51 -0300

    Agrega: tipado a los routes

--- src/navigation/routes.ts
+++ src/navigation/routes.ts
@@ -1,5 +1,13 @@
-export const Routes = {
-    HOMESCREEN : 'HomeScreen',
+ type RootStackParamList = {
+    LOGIN_SCREEN: string;
+    HOME_SCREEN: string;
+    USER_RESGISTER: string;
+  };
+
+  
+export const Routes: RootStackParamList = {
+    LOGIN_SCREEN: 'LoginScreen',
+    HOME_SCREEN : 'HomeScreen',
     USER_RESGISTER: 'UserRegister'
 
 }
