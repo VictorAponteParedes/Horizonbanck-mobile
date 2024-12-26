2dfc400bd8e7390fcb7db6a864e8fcb4b972279b
Victor Aponte <vaponte520@gmail.com>
2024-12-26 09:34:23 -0300

    Agrega: botones de tabnavigation

--- src/navigation/routes.ts
+++ src/navigation/routes.ts
@@ -2,12 +2,18 @@
     LOGIN_SCREEN: string;
     HOME_SCREEN: string;
     USER_RESGISTER: string;
+    BOTTOM_TABS: string,
+    DRAWER:string
+
   };
 
   
 export const Routes: RootStackParamList = {
     LOGIN_SCREEN: 'LoginScreen',
     HOME_SCREEN : 'HomeScreen',
-    USER_RESGISTER: 'UserRegister'
+    USER_RESGISTER: 'UserRegister',
+    BOTTOM_TABS: 'BottomTabs',
+     DRAWER: 'Drawer',
+
 
 }
--- src/navigation/tabNavigation.tsx
+++ src/navigation/tabNavigation.tsx
@@ -1,17 +1,18 @@
+import React from 'react';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import {Routes} from './routes';
 
-//Views
-import UserFormScreen from '../screens/Register';
+// Views
 import LoginScreen from '../screens/Login';
-import HomeScreen from '../screens/Home';
+import UserFormScreen from '../screens/Register';
+import ButtomNavigation from './buttomNatigation';
 
 const Stack = createNativeStackNavigator();
 const TabNavigation = () => {
   return (
     <Stack.Navigator initialRouteName={Routes.LOGIN_SCREEN}>
       <Stack.Screen name={Routes.LOGIN_SCREEN} component={LoginScreen} />
-      <Stack.Screen name={Routes.HOME_SCREEN} component={HomeScreen} />
+      <Stack.Screen name={Routes.HOME_SCREEN} component={ButtomNavigation} />
       <Stack.Screen name={Routes.USER_RESGISTER} component={UserFormScreen} />
     </Stack.Navigator>
   );
