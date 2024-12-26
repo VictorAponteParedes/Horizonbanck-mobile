812cb5600e430107ed1cd47f2fc082cb32f67614
Victor Aponte <vaponte520@gmail.com>
2024-12-26 09:34:52 -0300

    Agrega: exportacion de icono mastercard

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,12 +1,15 @@
 import facebookLogin from './svg/facebookLogin.svg'
 import horizonBank from './svg/horizonBankLogo.svg'
+
 import facebookLoginImagen from  './images/FacebookLogoImage.png'
 import googleLoginImagen from './images/GoogleLogoImagen.png'
+import masterCardLogo from './svg/card.svg'
 
 export {
     facebookLogin,
     facebookLoginImagen,
     googleLoginImagen,
-    horizonBank
+    horizonBank,
+    masterCardLogo
     
 };
