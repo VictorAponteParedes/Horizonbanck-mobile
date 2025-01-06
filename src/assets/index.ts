ba7ecc020be654797f667de7e797694f2f59a701
Victor Aponte <vaponte520@gmail.com>
2025-01-06 20:35:07 -0300

    Agrega:  elimina archivo que no exite

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,15 +1,13 @@
 import facebookLogin from './svg/facebookLogin.svg'
 import horizonBank from './svg/horizonBankLogo.svg'
 
-import facebookLoginImagen from  './images/FacebookLogoImage.png'
+import facebookLoginImagen from './images/FacebookLogoImage.png'
 import googleLoginImagen from './images/GoogleLogoImagen.png'
-import masterCardLogo from './svg/card.svg'
 
 export {
     facebookLogin,
     facebookLoginImagen,
     googleLoginImagen,
     horizonBank,
-    masterCardLogo
-    
+
 };
