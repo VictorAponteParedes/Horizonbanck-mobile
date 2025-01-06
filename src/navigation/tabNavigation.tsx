9872e3b37623fed529a7d3577e4296f7bea14686
Victor Aponte <vaponte520@gmail.com>
2025-01-06 20:46:13 -0300

    Agrega: se crea las vistas de transfer y configuracion

--- src/navigation/tabNavigation.tsx
+++ src/navigation/tabNavigation.tsx
@@ -1,18 +1,18 @@
 import React from 'react';
-import {createNativeStackNavigator} from '@react-navigation/native-stack';
-import {Routes} from './routes';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { Routes } from './routes';
 
 // Views
 import LoginScreen from '../screens/Login';
 import UserFormScreen from '../screens/Register';
-import ButtomNavigation from './buttomNatigation';
+import MyViewTabs from './ButtomTabNavigation';
 
 const Stack = createNativeStackNavigator();
 const TabNavigation = () => {
   return (
     <Stack.Navigator initialRouteName={Routes.LOGIN_SCREEN}>
       <Stack.Screen name={Routes.LOGIN_SCREEN} component={LoginScreen} />
-      <Stack.Screen name={Routes.HOME_SCREEN} component={ButtomNavigation} />
+      <Stack.Screen name={Routes.HOME_SCREEN} component={MyViewTabs} />
       <Stack.Screen name={Routes.USER_RESGISTER} component={UserFormScreen} />
     </Stack.Navigator>
   );
---
+++ src/screens/transfer/index.tsx
@@ -0,0 +1,16 @@
+import { View, Text } from "react-native";
+
+
+const TransferScreen = () => {
+
+
+    return (
+        <View>
+            <Text>
+                tranferir dinero
+            </Text>
+        </View>
+    )
+}
+
+export default TransferScreen;
