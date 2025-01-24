0afececd1399df981c8f7c0b04176a2b6391125d
Victor Aponte <vaponte520@gmail.com>
2025-01-24 17:31:04 -0300

    Agrega: service obtener users

--- src/services/auth.tsx
+++ src/services/auth.tsx
@@ -4,11 +4,12 @@ import { UserData } from '../@types'
 
 
 const AuthService = {
-    registerUser: async function (userdata: UserData) {
-
-        return await urlBase.post('/users', userdata)
-    }
-
-}
+  registerUser: async function (userdata: UserData) {
+    return await urlBase.post('/users', userdata);
+  },
+  getAllUsers: async function () {
+    return await urlBase.get('/users');
+  },
+};
 
 export default AuthService;
